cf03e9302353f712d43d1d7b7639cc8dcc275876
Evan Norton <evan@retro-mmo.com>
2024-05-07 00:37:54 -0400

    Add InitialUpdate.isSubscribed

--- src/updates/InitialUpdate.ts
+++ src/updates/InitialUpdate.ts
@@ -2,6 +2,7 @@ import { MainState } from "../MainState";
 import { Savefile } from "../Savefile";
 
 export interface InitialUpdate {
+  readonly isSubscribed: boolean;
   readonly mainState: MainState;
   readonly savefile: Savefile;
 }
