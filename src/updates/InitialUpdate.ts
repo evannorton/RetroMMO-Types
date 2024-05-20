854f43647591f61b954d8460ca09d79d5dddd27e
Evan Norton <evan@retro-mmo.com>
2024-05-20 00:52:00 -0400

    Add InitialWorldUpdate

--- src/updates/InitialUpdate.ts
+++ src/updates/InitialUpdate.ts
@@ -1,8 +1,12 @@
 import { MainState } from "../MainState";
 import { Savefile } from "../Savefile";
 
+export interface InitialWorldUpdate {
+  readonly characterID: string;
+}
 export interface InitialUpdate {
   readonly isSubscribed: boolean;
   readonly mainState: MainState;
   readonly savefile: Savefile;
+  readonly world?: InitialWorldUpdate;
 }
