57ff995c8507faa8ce6ef04519c1ded6158d76db
Evan Norton <evan@retro-mmo.com>
2024-05-20 00:11:13 -0400

    Add stuff

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,5 @@
 export interface Constants {
   "free-character-slots": number;
   "paid-character-slots": number;
+  "tile-size": number;
 }
--- src/definitions/TilemapDefinition/TilemapDefinition.ts
+++ src/definitions/TilemapDefinition/TilemapDefinition.ts
@@ -3,6 +3,8 @@ import { TilemapTileDefinition } from "./TilemapTileDefinition";
 import { TilemapTilesetDefinition } from "./TilemapTilesetDefinition";
 
 export interface TilemapDefinition extends Definition {
+  readonly height: number;
   readonly tiles: TilemapTileDefinition[][];
   readonly tilesets: TilemapTilesetDefinition[];
+  readonly width: number;
 }
