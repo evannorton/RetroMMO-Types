af2cf04823775d4b8987f7fdfbd0e1f38dc5bf3e
Evan Norton <evan@retro-mmo.com>
2024-05-19 20:45:05 -0400

    Add height to TilesetDefinition

--- src/definitions/TilesetDefinition/TilesetDefinition.ts
+++ src/definitions/TilesetDefinition/TilesetDefinition.ts
@@ -2,6 +2,7 @@ import { Definition } from "../Definition";
 import { TilesetTileDefinition } from "./TilesetTileDefinition";
 
 export interface TilesetDefinition extends Definition {
+  readonly height: number;
   readonly tiles: TilesetTileDefinition[][];
   readonly width: number;
 }
