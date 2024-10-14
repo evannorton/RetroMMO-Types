fca935242b0f63b75dd454242e08ac2fb47e0cde
Evan Norton <evan@retro-mmo.com>
2024-10-13 22:58:33 -0500

    Add order to WorldMoveCharacterUpdate

--- src/updates/world/WorldMoveCharacterUpdate.ts
+++ src/updates/world/WorldMoveCharacterUpdate.ts
@@ -2,5 +2,6 @@ import { Direction } from "../../Direction";
 
 export interface WorldMoveCharacterUpdate {
   readonly direction: Direction;
+  readonly order: number;
   readonly worldCharacterID: string;
 }
