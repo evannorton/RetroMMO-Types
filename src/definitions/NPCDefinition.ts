a3d4889ffd5b231a2c136f90a372576874a0a8be
Evan Norton <evan@retro-mmo.com>
2024-11-12 04:32:20 -0600

    Add data to NPCDefinition

--- src/definitions/NPCDefinition.ts
+++ src/definitions/NPCDefinition.ts
@@ -3,7 +3,11 @@ import { Direction } from "../Direction";
 
 export interface NPCDefinition extends Definition {
   readonly actorImageSourceID: string;
+  readonly dialogue?: string;
+  readonly encounterID?: string;
   readonly indicatorImageSourceID: string;
+  readonly innCost?: number;
   readonly name: string;
+  readonly shopID?: string;
   readonly startDirection: Direction;
 }
