8520cd27cee5f678c9565f2428aadb31be9a004c
Evan Norton <evan@retro-mmo.com>
2024-11-12 01:28:00 -0600

    Add order to WorldPartyCharacterUpdate

--- src/updates/world/WorldPartyCharacterUpdate.ts
+++ src/updates/world/WorldPartyCharacterUpdate.ts
@@ -2,6 +2,7 @@ import { WorldResourcesUpdate } from "./WorldResourcesUpdate";
 
 export interface WorldPartyCharacterUpdate {
   readonly openedChestIDs: readonly string[];
+  readonly order: number;
   readonly resources: WorldResourcesUpdate;
   readonly worldCharacterID: string;
 }
