61424cf3720b76cfb715018ee718090dc2d44169
Evan Norton <evan@retro-mmo.com>
2024-12-11 09:13:53 -0600

    Add MonsterDefinition.name

--- src/definitions/MonsterDefinition.ts
+++ src/definitions/MonsterDefinition.ts
@@ -4,6 +4,7 @@ export interface MonsterDefinition extends Definition {
   readonly battleHeight: number;
   readonly battleWidth: number;
   readonly imageSourceID: string;
+  readonly name: string;
   readonly offset: number;
   readonly shadowXOffset: number;
   readonly shadowXRadius: number;
