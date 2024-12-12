630f21eff30c37f8e771dd3265724d8780cfc93e
Evan Norton <evan@retro-mmo.com>
2024-12-12 01:19:19 -0600

    Add QuestDefinition experience and gold

--- src/definitions/QuestDefinition.ts
+++ src/definitions/QuestDefinition.ts
@@ -1,6 +1,8 @@
 export interface QuestDefinition {
   readonly availableText: string;
   readonly completedText: string;
+  readonly experience: number;
+  readonly gold: number;
   readonly inProgressText: string;
   readonly monster?: {
     readonly kills: number;
