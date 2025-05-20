3dd3db03f65e49503d9e36b953ebd7672f55fd24
Evan Norton <evan@retro-mmo.com>
2025-05-20 02:17:05 -0500

    Battle types

--- src/battle-events/BattleEvent.ts
+++ src/battle-events/BattleEvent.ts
@@ -1,6 +1,8 @@
+import { BattleEventType } from "./BattleEventType";
+
 export interface BattleEvent {
   readonly channel: number;
   readonly duration: number;
   readonly startedAt: number;
-  readonly type: string;
+  readonly type: BattleEventType;
 }
--- src/battle-events/BattleUseAbilityEvent.ts
+++ src/battle-events/BattleUseAbilityEvent.ts
@@ -1,9 +1,17 @@
 import { BattleEvent } from "./BattleEvent";
 import { BattleEventType } from "./BattleEventType";
 
+export interface BattleUseAbilityEventCaster {
+  readonly battlerID: string;
+  readonly name: string;
+}
+export interface BattleUseAbilityEventTarget {
+  readonly battlerID: string;
+  readonly name: string;
+}
 export interface BattleUseAbilityEvent extends BattleEvent {
   readonly abilityID: string;
-  readonly casterBattlerID: string;
-  readonly targetBattlerID?: string;
+  readonly caster: BattleUseAbilityEventCaster;
+  readonly target?: BattleUseAbilityEventTarget;
   readonly type: BattleEventType.UseAbility;
 }
