a79dc9fac25d61addd81a8982313ac73c65a637e
Evan Norton <evan@retro-mmo.com>
2025-07-12 15:31:38 -0500

    Add BattleRoundUpdate.duration and BattleRoundUpdate.isFinal

--- src/updates/battle/BattleRoundUpdate.ts
+++ src/updates/battle/BattleRoundUpdate.ts
@@ -1,6 +1,8 @@
 import { BattleEvent } from "../../battle-events/BattleEvent";
 
 export interface BattleRoundUpdate {
+  readonly duration: number;
   readonly events: readonly BattleEvent[];
+  readonly isFinal?: boolean;
   readonly serverTime: number;
 }
