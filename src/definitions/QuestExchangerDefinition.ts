7c560eca84e89fd904ea68a3ed94f600768f197f
Evan Norton <evan@retro-mmo.com>
2025-08-20 15:19:12 -0500

    Add QuestExchangerDefinition questExchangerQuests isGiver and isReceiver

--- src/definitions/QuestExchangerDefinition.ts
+++ src/definitions/QuestExchangerDefinition.ts
@@ -1,5 +1,7 @@
 export interface QuestExchangerDefinition {
   readonly questExchangerQuests: readonly {
+    readonly isGiver?: boolean;
+    readonly isReceiver?: boolean;
     readonly questID: string;
   }[];
 }
