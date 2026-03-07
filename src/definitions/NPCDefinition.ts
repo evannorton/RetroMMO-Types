46f26c2dfe917397bad0d39193b80ae517fc8506
Evan Norton <evan@retro-mmo.com>
2026-03-07 17:11:06 -0600

    Remove NPCDefinition.indicatorImagePath

--- src/definitions/NPCDefinition.ts
+++ src/definitions/NPCDefinition.ts
@@ -5,7 +5,6 @@ export interface NPCDefinition extends Definition {
   readonly actorImagePath: string;
   readonly dialogue?: string;
   readonly encounterID?: string;
-  readonly indicatorImagePath: string;
   readonly innCost?: number;
   readonly name: string;
   readonly questExchangerID?: string;
