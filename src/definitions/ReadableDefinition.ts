0a2beb698db7fc13214ef12551ac23b00dd10fc7
Evan Norton <evan@retro-mmo.com>
2026-05-26 14:27:31 -0500

    User Color for ReadableDefinition.color

--- src/definitions/ReadableDefinition.ts
+++ src/definitions/ReadableDefinition.ts
@@ -1,7 +1,8 @@
+import { Color } from "../Color";
 import { Definition } from "./Definition";
 
 export interface ReadableDefinition extends Definition {
-  readonly color: string;
+  readonly color: Color;
   readonly contents: string;
   readonly height: number;
   readonly horizontalAlignment: "center" | "left" | "right";
