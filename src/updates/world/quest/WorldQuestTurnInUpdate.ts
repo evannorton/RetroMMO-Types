848b5fdc259b5d0a8ccfc72df8cc09739d85bac9
Evan Norton <evan@retro-mmo.com>
2026-06-17 20:48:25 -0500

    Add WorldQuestTurnInWorldUpdate gold and stat fields

--- src/updates/world/quest/WorldQuestTurnInUpdate.ts
+++ src/updates/world/quest/WorldQuestTurnInUpdate.ts
@@ -5,10 +5,17 @@ export interface WorldQuestTurnInWorldCharacterUpdate {
   readonly resources: WorldResourcesUpdate;
 }
 export interface WorldQuestTurnInWorldUpdate {
+  readonly agility: number;
   readonly characters: readonly WorldQuestTurnInWorldCharacterUpdate[];
+  readonly defense: number;
   readonly experienceUntilLevel?: number;
+  readonly intelligence: number;
+  readonly inventoryGold: number;
+  readonly luck: number;
   readonly npcID: string;
   readonly questID: string;
+  readonly strength: number;
+  readonly wisdom: number;
 }
 export interface WorldQuestTurnInPlayerUpdate {
   readonly level: number;
